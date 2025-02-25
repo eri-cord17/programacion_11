9370f6825fde406ab426ca543eacd35e234a5b91
eri.cord17 <ecanales@ccsanvito.ed.cr>
2025-02-24 18:52:48 -0600

    Guardando mi proyecto

---
+++ calculadora.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+    
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+    
+    cout << "Suma: " << num1 + num2 << endl;
+    cout << "Multiplicacion: " << num1 * num2 << endl;
+    
+    return 0;
+}
